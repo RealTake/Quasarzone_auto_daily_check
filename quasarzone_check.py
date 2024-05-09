150c795b4d84d43dc9981226c759f64efd82740e
wonsang.choi <wonsang.choi@e4net.net>
2024-05-09 11:04:37 +0900

    FIX: id 미입력 오류 현상 수정

--- quasarzone_check.py
+++ quasarzone_check.py
@@ -11,9 +11,12 @@ from selenium.webdriver.support import expected_conditions as EC
 id = sys.argv[1]
 pwd = sys.argv[2]
 
+print("id", id)
+print("pwd:", pwd)
+
 options = Options()
 
-prefs = {"download.default_directory": download_path, 'safebrowsing.enabled': 'True'}
+prefs = {'safebrowsing.enabled': 'True'}
 options.add_experimental_option("prefs", prefs)
 options.add_argument("--disable-notifications")
 options.add_argument("--start-maximized")
@@ -27,8 +30,9 @@ def click_by_js(xpath):
     javaScript = """document.evaluate('{0}', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()""".format(xpath)
 
     driver.execute_script(javaScript)
-
+# try:
 driver.get("https://quasarzone.com/login")
+sleep(3)
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "login_id")))
 driver.find_element_by_id("login_id").send_keys(id)
 driver.find_element_by_id("password").send_keys(pwd)
@@ -37,6 +41,6 @@ sleep(3)
 driver.get("https://quasarzone.com/users/attendance")
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "active2")))
 driver.find_element_by_css_selector(".active2").click()
-
-driver.close()
-driver.quit()
+# finally:
+    # driver.close()
+    # driver.quit()
